4d5485a9b193cfafb9939dfe7c6d421b022f81df
sununshine <1274048194@qq.com>
2019-12-31 15:19:26 +0800

    提交项目

---
+++ src/main/java/com/goldenhome/GoldenhomeCloudapiApplication.java
@@ -0,0 +1,13 @@
+package com.goldenhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenhomeCloudapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenhomeCloudapiApplication.class, args);
+	}
+
+}
